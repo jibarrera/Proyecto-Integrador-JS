4ff7e2c1797df362da1b129bb854f1f835ddf480
José Iván Barrera <51324038+jibarrera@users.noreply.github.com>
2024-10-22 15:46:30 -0300

    Proyecto Integrador JavaScript

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+/*
+const costoBase = 1.16;
+
+alert("Las empresas de seguro poseen un valor definido por Costo Base por metro cuadrado que es de USD 1,16.");
+
+let m2 = prompt("Ingrese los metros cuadrados de la vivienda: ");
+
+let fm = 1.07;
+
+var montoTotalPoliza = costoBase*m2*fm
+
+alert("El monto estimado de la póliza es de : $ "+ montoTotalPoliza);
